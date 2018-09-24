a423a31035228bbd80b637cef5a41009fa285e3e
joshe <jose@patagonia-it.com>
2018-09-24 17:52:59 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import {MzButtonModule, MzInputModule, MzNavbarModule} from 'ngx-materialize';
+import { MzCardModule } from 'ngx-materialize';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MzButtonModule,
+    MzInputModule,
+    MzCardModule,
+    MzNavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
