86fa78c598c1fc7bd13fde5c03ca671b65609df8
jvillane <jvillane@gmail.com>
2018-09-25 10:39:43 -0300

    DONE: versión inicial usando materialize css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {MzButtonModule, MzInputModule, MzNavbarModule} from 'ngx-materialize';
-import {MzCardModule} from 'ngx-materialize';
 import {ContribucionesPaso1Component} from './contribuciones-paso1/contribuciones-paso1.component';
-import {AppRoutingModule} from './/app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {CertificadosComponent} from './certificados/certificados.component';
 
 @NgModule({
@@ -16,10 +14,6 @@ import {CertificadosComponent} from './certificados/certificados.component';
   ],
   imports: [
     BrowserModule,
-    MzButtonModule,
-    MzInputModule,
-    MzCardModule,
-    MzNavbarModule,
     AppRoutingModule
   ],
   providers: [],
