61da9ab6b98ee36dfc447dc58ba6d6a5d937983a
joshe <jose@patagonia-it.com>
2019-03-04 14:19:58 -0300

    ADD: environment para desarrollo

---
+++ src/environments/environment.ts
@@ -0,0 +1,142 @@
+const endpoints = {
+  publica: 'https://9l70yekz53.execute-api.us-east-1.amazonaws.com/dev',
+  privada: 'https://9l70yekz53.execute-api.us-east-1.amazonaws.com/dev',
+  lambdaRoles: '',
+  elastic: 'https://w2jmtnip5c.execute-api.us-east-1.amazonaws.com/dev',
+};
+
+const urlsBase = {
+  publica: endpoints.publica + '/proxy-public',
+  privada: endpoints.privada + '/proxy-private',
+  elastic: endpoints.elastic,
+  lambdaRoles: endpoints.lambdaRoles + '/roles'
+};
+
+const id = {
+  authorizeURL: "https://autentica-dev.tegere.info/oauth2/authorize",
+  clientId: "178vr805sl67cfm1u83m3vtv3c",
+  redirectURI: "http://localhost:4201/login"
+};
+
+const pathBase = {
+  clienteBienRaiz: '/ClienteBienRaizWS/api/BienRaiz',
+  deudaRol: '/RecuperaDeudaROLRS/api/bienraiz',
+  bienRaiz: '/BienRaizWS/api/BienRaiz'
+};
+
+
+export const environment = {
+  snackbarTime: 5000,
+  production: true,
+  cuentaUrl: id.authorizeURL + "?response_type=token&client_id=" + id.clientId + "&redirect_uri=" + id.redirectURI,
+  pago: {
+    url: 'http://test2.tesoreria.cl/ConsultaTipoDeudaWEB/enviaMedioPago'
+  },
+  cognito: {
+    authorizeURL: 'https://autentica-dev.tegere.info/oauth2/authorize',
+    logoutURL: '',
+    clientId: '3eqo995p7hord0t8vsdma4r8a8',
+    redirectUri: 'http://localhost:4202/login',
+    logoutUri: '',
+    jwtCookieName: 'tgr-jwt',
+    expCookieName: 'exp',
+    allowCookies: true,
+  },
+  sizeResultSuggested: 10,
+  sizeResultPage: 30,
+  certificados: {
+    anoDesde: 1955,
+  },
+  dialogoRecuperarPropiedadesEmail: false,
+  lambda: {
+    enviarMailCodigoVerificacion: {
+      url: urlsBase.lambdaRoles,
+      path: '/codigorecuperacion/enviar',
+      method: 'POST'
+    },
+    validarCodigo: {
+      url: urlsBase.lambdaRoles,
+      path: '/codigorecuperacion/validar',
+      method: 'POST'
+    },
+    recuperar: {
+      url: urlsBase.lambdaRoles,
+      path: '/recuperar',
+      method: 'POST'
+    },
+    asociar: {
+      url: urlsBase.lambdaRoles,
+      path: '/asociar',
+      method: 'POST'
+    },
+    desasociar: {
+      url: urlsBase.lambdaRoles,
+      path: '/desasociar',
+      method: 'POST'
+    }
+  },
+  servicios: {
+    certificadoDeudas: {
+      url: urlsBase.publica,
+      path: '/tgrSuscriptorWs/api/suscriptor/certificado/obtener',
+      method: 'POST'
+    },
+    certificadoHistorialPago: {
+      url: urlsBase.publica,
+      path: '/recaPagoConsultasWS/api/pago/consulta',
+      method: 'POST'
+    },
+    obtenerBienRaizAsociado: {
+      url: urlsBase.privada,
+      path: pathBase.clienteBienRaiz + '/asociado/obtener',
+      method: 'GET'
+    },
+    obtenerBienRaizNoAsociado: {
+      url: urlsBase.privada,
+      path: pathBase.clienteBienRaiz + '/noasociado/obtener',
+      method: 'GET'
+    },
+    asociarBienRaiz: {
+      url: urlsBase.privada,
+      path: pathBase.clienteBienRaiz + '/bienraiz/asociar',
+      method: 'POST'
+    },
+    desasociarBienRaiz: {
+      url: urlsBase.privada,
+      path: pathBase.clienteBienRaiz + '/bienraiz/desasociar',
+      method: 'POST'
+    },
+    recuperarDeudaRol: {
+      url: urlsBase.publica,
+      path: pathBase.deudaRol + '/deuda/rol/obtiene',
+      method: 'POST'
+    },
+    buscarBienRaiz: {
+      url: urlsBase.publica,
+      path: pathBase.bienRaiz + '/bienraiz/obtener/rolin',
+      method: 'POST'
+    }
+  },
+  elastic: {
+    localidad: {
+      url: urlsBase.elastic + '/search/localidad',
+      method: 'POST',
+      body: {
+        size: 400,
+        from: 0
+      }
+    },
+    tiposPropiedades: {
+      url: urlsBase.elastic + '/search/tipo_propiedad',
+      method: 'POST',
+      body: {
+        size: 400,
+        from: 0
+      }
+    },
+    propiedades: {
+      url: urlsBase.elastic + '/search/propiedad',
+      method: 'POST'
+    },
+  }
+};
