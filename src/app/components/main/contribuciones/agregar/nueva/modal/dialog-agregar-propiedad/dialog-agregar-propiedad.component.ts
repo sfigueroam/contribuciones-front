3db41bfb7fa1e810d825a2761da4081a25186f4e
joshe <jose@patagonia-it.com>
2019-03-25 16:38:26 -0300

    WIP: modal

---
+++ src/app/components/main/contribuciones/agregar/nueva/modal/dialog-agregar-propiedad/dialog-agregar-propiedad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-agregar-propiedad',
+  templateUrl: './dialog-agregar-propiedad.component.html',
+  styleUrls: ['./dialog-agregar-propiedad.component.scss']
+})
+export class DialogAgregarPropiedadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
