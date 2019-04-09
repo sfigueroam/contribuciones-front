547a04f07888a85a5f980ba9d07cdeb0b0bf4431
Daniela Meza Andrade <dmezaa@tgr.cl>
2019-04-09 22:12:19 +0000

    mejoras pantalla carro y agregar

--- src/app/domain/TgrReCaptcha.ts
+++ src/app/domain/TgrReCaptcha.ts
@@ -1,7 +1,7 @@
 export class TgrReCaptcha{
 
   public siteKey = '';
-  public badge: 'bottomright' | 'bottomleft' | 'inline' = 'bottomleft';
+  public badge: 'bottomright' | 'bottomleft' | 'inline' = 'inline';
   public type: 'image' | 'audio';
   public theme: 'light' | 'dark' = 'light';
   public action = '';
