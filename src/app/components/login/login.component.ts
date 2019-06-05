b531016810959dc1e01619b043a6c455571bc1b7
jmora <jmora@tgr.cl>
2019-06-05 12:59:09 +0000

    pruebas para manejo de datos cognito

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,11 @@ import {CognitoService} from '../../services/cognito.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user.service';
 
+//obtener datos del jwt (nombre para uso en la sesion)
+import * as jwt_decode from 'jwt-decode';
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
@@ -12,6 +17,7 @@ export class LoginComponent implements OnInit {
   identity: any;
 
   exp: Date;
+  
 
   constructor(private cognito: CognitoService, private route: ActivatedRoute, private router: Router, private user: UserService) {
     this.cognito.login(route.snapshot.fragment).then(
