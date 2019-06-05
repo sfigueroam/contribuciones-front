ba68c4eb3004bb4a40609b4c1de19bcdf25421cc
jmora <jmora@tgr.cl>
2019-06-05 20:53:54 +0000

    prueba con @input

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit, ViewContainerRef} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewContainerRef} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user.service';
 import {CognitoService} from '../../services/cognito.service';
@@ -16,6 +16,8 @@ export class MainComponent implements OnInit, AfterViewInit {
   logged: boolean;
   index: number;
   isActiveLogin: boolean;
+  @Input() public name: string;
+
 
   constructor(route: ActivatedRoute,
               private router: Router,
