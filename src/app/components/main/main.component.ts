18a83820ab11926a766209395eae25416fe3a9c5
jmora <jmora@tgr.cl>
2019-06-05 21:58:31 +0000

    prueba con @input

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -5,7 +5,6 @@ import {CognitoService} from '../../services/cognito.service';
 import {MdlDialogOutletService, MdlDialogService} from '@angular-mdl/core';
 import {environment} from '../../../environments/environment';
 import {AsociarCorreoComponent} from '../dialogs/asociar-correo/asociar-correo.component';
-import {LoginComponent} from '../login/login.component';
 
 
 
@@ -19,8 +18,6 @@ export class MainComponent implements OnInit, AfterViewInit {
   logged: boolean;
   index: number;
   isActiveLogin: boolean;
-  //input de nombre de usuario
-  @Input() name: string;
   constructor(route: ActivatedRoute,
               private router: Router,
               private user: UserService,
