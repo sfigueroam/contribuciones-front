347beb428446255c9201f8091c8818d50360ccfb
jmora <jmora@tgr.cl>
2019-07-09 16:14:38 +0000

    prueba environment variables en js snipped

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import {googleAnalyticsHeadScripts} from './assets/googleAnalytics/script';
+import {newRelicHeadScripts} from './assets/newRelic/script';
 
 if (environment.production) {
   enableProdMode();
 }
 
-googleAnalyticsHeadScripts();
+newRelicHeadScripts();
+googleAnalyticsHeadScripts(); 
+
+
+
+
 
 
 
