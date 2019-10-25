4e1ad614edf8e0e1af36217e448b0202ec8ad3d1
jmora <jmora@tgr.cl>
2019-10-25 13:38:30 +0000

    prueba nuevos canales

--- src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
+++ src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
@@ -18,30 +18,15 @@ export class BotonPagarComponent implements OnInit {
   codigos: string;
 
   urlPagoTgr: string;
-  user_conex: string;
-  sinClave: boolean = false;
-  claveTgr: boolean = false;
-  claveUnica: boolean = false;
-
+  
   constructor(public deviceDetectService: DeviceDetectService,
               private userdataservice: UserDataService) {
   }
 
+
+
   ngOnInit() {
-    this.user_conex = this.userdataservice.conex_usuario;
-    console.log(this.user_conex);
-    if (this.user_conex == "") {
-      this.sinClave = true;
-      console.log("sin clave" || this.sinClave);
-    }
-    if (this.user_conex == "ClaveTesoreria"){
-      this.claveTgr = true;
-      console.log("sin clave" || this.claveTgr);
-    }
-    if (this.user_conex == "ClaveUnica"){
-      this.claveUnica = true;
-      console.log("sin clave" || this.claveUnica);
-    }
+
     
     //this.urlPagoTgr = environment.pago.url;
     
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -9,9 +9,31 @@ export class UserDataService {
   //javier
   deudaNoLiquidable: boolean;
   conex_usuario: string;
-  
+  sinClave: boolean = false;
+  claveTgr: boolean = false;
+  claveUnica: boolean = false;
   
   constructor() { }
   
+  conexion_Usuario(){
+    
+    if (this.conex_usuario == "") {
+      this.sinClave = true;
+      return this.sinClave;
+      console.log("sin clave" || this.sinClave);
+    }
+    if (this.conex_usuario == "ClaveTesoreria"){
+      this.claveTgr = true;
+      return this.claveTgr;
+      console.log("sin clave" || this.claveTgr);
+    }
+    if (this.conex_usuario == "ClaveUnica"){
+      this.claveUnica = true;
+      return this.claveUnica;
+      console.log("sin clave" || this.claveUnica);
+    }
+    
+  }
+    
 
 }
