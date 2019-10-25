11c0f95a3aa5f49f8dc96c7881802f7a6286b69a
jmora <jmora@tgr.cl>
2019-10-25 15:57:53 +0000

    prueba de canal

--- src/app/services/canal-service.service.ts
+++ src/app/services/canal-service.service.ts
@@ -14,18 +14,4 @@ export class CanalServiceService{
               private userdataservice: UserDataService){
               }
     
-  
-    detectaConexion():void{
-      this.conex_usuario = this.userdataservice.conex_usuario;
-      if (this.conex_usuario == "") {
-        this.reg = "SC";
-      }
-      if (this.conex_usuario == "ClaveTesoreria"){
-        this.reg = "CT";
-      }
-      if (this.conex_usuario == "ClaveUnica"){
-        this.reg = "CU";
-      }
-      
-    }
 }
