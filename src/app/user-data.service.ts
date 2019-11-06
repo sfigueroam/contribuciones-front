21260025cffef2e3a16aad80e701465798ab663c
jmora <jmora@tgr.cl>
2019-11-06 17:06:41 +0000

    se deshacen los cambios de canal

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -8,23 +8,23 @@ export class UserDataService {
   nombre_usuario: string;
   //javier
   deudaNoLiquidable: boolean;
-  conex_usuario: string;
-  reg: string;
+  // conex_usuario: string;
+  // reg: string;
   
   constructor() { }
   
-      detectaConexion():void{
-      if (this.conex_usuario == "") {
-        this.reg = "SC";
-      }
-      if (this.conex_usuario == "ClaveTesoreria"){
-        this.reg = "CT";
-      }
-      if (this.conex_usuario == "ClaveUnica"){
-        this.reg = "CU";
-      }
+    //   detectaConexion():void{
+    //   if (this.conex_usuario == "") {
+    //     this.reg = "SC";
+    //   }
+    //   if (this.conex_usuario == "ClaveTesoreria"){
+    //     this.reg = "CT";
+    //   }
+    //   if (this.conex_usuario == "ClaveUnica"){
+    //     this.reg = "CU";
+    //   }
       
-    }
+    // }
   
     
 
