e018190f1a32ee21ba2763d57cf3be46fee2da82
jmora <jmora@tgr.cl>
2019-11-07 17:33:37 +0000

    prueba de valores de conexion

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -27,12 +27,16 @@ export class LoginComponent implements OnInit {
     this.cognito.login(route.snapshot.fragment).then(
       value => {
         this.identity = value;
-        this.name = value.name;
+        //this.name = value.name;
         this.provider_array = value.identities[0];
         this.provider = this.provider_array.providerName;
         
         if(this.name == undefined || this.name == null){
-          this.name = value.username;
+          this.name = "";
+        }
+        // revisar el despliegue del nombre en qa
+        if (this.provider == "ClaveUnica"){
+          this.name = value.name['nombres'][0];
         }
         this.user.getBienesRaices().then(
           () => this.router.navigate(['/main/contribuciones/seleccionar-cuotas']),
@@ -55,6 +59,7 @@ export class LoginComponent implements OnInit {
     //this.userdataservice.conex_usuario = this.provider;
     console.log(this.identity);
     console.log('provider : ' || this.provider);
+    console.log('name: ' || this.name);
     
     
   }
