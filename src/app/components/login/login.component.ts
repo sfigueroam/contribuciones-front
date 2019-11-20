5798913d9ac0802fedc45e846f4358361ec98016
jmora <jmora@tgr.cl>
2019-11-20 15:42:06 +0000

    cambio modo captura

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
     this.cognito.login(route.snapshot.fragment).then(
       value => {
         this.identity = value;
-        this.name = value.name['nombres'][0];
+        this.name = value.name[0];
         this.provider_array = value.identities[0];
         this.provider = this.provider_array.providerName;
         
