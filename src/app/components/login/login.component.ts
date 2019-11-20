9d4ff17770aaa631d8cea7c5cb87844c47eb7707
jmora <jmora@tgr.cl>
2019-11-20 15:30:47 +0000

    cambio modo captura

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
     this.cognito.login(route.snapshot.fragment).then(
       value => {
         this.identity = value;
-        this.name = value.name;
+        this.name = value.name.nombres[0];
         this.provider_array = value.identities[0];
         this.provider = this.provider_array.providerName;
         
@@ -38,10 +38,10 @@ export class LoginComponent implements OnInit {
           this.name = "";
         }
         // revisar el despliegue del nombre en qa
-        if (this.provider == "ClaveUnica"){
-          this.name = value.name.nombres[0];
-          // this.name = value.name;
-        }
+        // if (this.provider == "ClaveUnica"){
+        //   this.name = value.name.nombres[0];
+        //   // this.name = value.name;
+        // }
         this.user.getBienesRaices().then(
           () => this.router.navigate(['/main/contribuciones/seleccionar-cuotas']),
           (err) => {
