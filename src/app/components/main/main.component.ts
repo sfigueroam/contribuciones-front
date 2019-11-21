ec65808c4023bdf4f78247ce4a58b2fac7cef573
jmora <jmora@tgr.cl>
2019-11-21 18:23:57 +0000

    descomenta nombre de usuario en manin

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -54,18 +54,13 @@ export class MainComponent implements OnInit, AfterViewInit {
   ngOnInit() {
   
    this.logged = this.user.isLogged();
-   this.usuariologin = this.userdataservice.nombre_usuario;
-  // console.log(this.usuariologin);
-    // if (this.userdataservice.nombre_usuario === '' || this.userdataservice.nombre_usuario === null || this.userdataservice.nombre_usuario === undefined){
-    //   this.usuariologin = '';
-    // }   else{
-    //     this.usuariologinmay = this.userdataservice.nombre_usuario.split(' ')[0];
-    //     this.usuario1 = this.usuariologinmay.substring(0,1);
-    //     this.usuario2 = this.usuariologinmay.substring(1,100);
-    //     this.usuario2min = this.usuario2.toLowerCase();
-    //     this.usuariologin = this.usuario1.concat(this.usuario2min);
-
-    //   }
+  // this.usuariologin = this.userdataservice.nombre_usuario;
+    if (this.userdataservice.nombre_usuario != ''){
+        this.usuario1 = this.userdataservice.nombre_usuario.substring(0,1);
+        this.usuario2 = this.userdataservice.nombre_usuario.substring(1,100);
+        this.usuario2min = this.usuario2.toLowerCase();
+        this.usuariologin = this.usuario1.concat(this.usuario2min);
+      }
  
     
   }
