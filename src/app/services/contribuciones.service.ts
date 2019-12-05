bb700dbdb40466ec82685ef40fc2196649e7814e
jmora <jmora@tgr.cl>
2019-12-05 19:29:26 +0000

    se agrega tag manager

--- src/app/services/contribuciones.service.ts
+++ src/app/services/contribuciones.service.ts
@@ -77,7 +77,7 @@ export class ContribucionesService {
       }
     );
   }
-
+// lista propiedades con toda su data
   getBienesRaices(rut: number): Promise<Propiedad[]> {
 
     if (this.propiedades) {
@@ -124,7 +124,8 @@ export class ContribucionesService {
       }
     }
   }
-
+//JMS: Llamado a servicio que carga la deuda del rol 
+// getDeudaByRol llama la cuota (servicio antiguo)
   private cargarRol(rol: Rol): Promise<any> {
     if (rol.cuotas.length > 0) {
       return new Promise((resolve, reject) => {
@@ -169,7 +170,7 @@ export class ContribucionesService {
     obtenerBienRaizAsociado.url = obtenerBienRaizAsociado.url + '/' + rut;
     return this.requestService.request(obtenerBienRaizAsociado);
   }
-
+//JMS: Servicio específico que carga la deuda del rol
   private getDeudaByRol(rol, cuotas?: any): Promise<{}> {
     const body = {
       'idRol': rol,
--- src/assets/googleAnalytics/script.js
+++ src/assets/googleAnalytics/script.js
@@ -2,17 +2,18 @@ import {environment} from "../../environments/environment";
 
 export function googleAnalyticsHeadScripts() {
   const head = document.getElementsByTagName('head')[0];
+  const body = document.getElementsByTagName('body')[0];
 
   const googleAnalyticsFirstScript = document.createElement('script');
   googleAnalyticsFirstScript.async = true;
-  googleAnalyticsFirstScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.googleAnalyticsCode;
+  googleAnalyticsFirstScript.src = 'https://www.googletagmanager.com/ns.html?id=' + environment.googleAnalyticsCode;
 
   const googleAnalyticsSecondScript = document.createElement('script');
-  googleAnalyticsSecondScript.innerHTML = '    window.dataLayer = window.dataLayer || [];\n' +
-    '    function gtag(){dataLayer.push(arguments);}\n' +
-    '    gtag(\'js\', new Date());\n' +
-    '\n' +
-    '    gtag(\'config\', \'' + environment.googleAnalyticsCode + '\');';
+  googleAnalyticsSecondScript.innerHTML = ' (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n'+
+  'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n'+
+  'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n'+
+  '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n'+
+  '})(window,document,\'script\',\'dataLayer\',\'' + environment.googleAnalyticsCode + '\');';
 
   head.insertBefore(googleAnalyticsSecondScript, head.firstChild);
   head.insertBefore(googleAnalyticsFirstScript, head.firstChild);
