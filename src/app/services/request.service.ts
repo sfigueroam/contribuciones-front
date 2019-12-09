4306b12f3ee00054903de793b44ea6c52a945296
jmora <jmora@tgr.cl>
2019-12-09 19:14:48 +0000

    determinar el error

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -72,7 +72,7 @@ export class RequestService {
 
     return new Promise((resolve, reject) => {
       this.http.request(servicio.method,
-        'https://9l70yekz53.execute-api.us-east-1.amazonaws.com/dev/servicios-recaudacion/v1/liquidacion/deudasrol/1900102013',
+        'https://9l70yekz53.execute-api.us-east-1.amazonaws.com/dev/servicios-recaudacion/v1/liquidacion/deudasrol?rol=1900102013',
         {
           body: rol,
           responseType: 'json',
