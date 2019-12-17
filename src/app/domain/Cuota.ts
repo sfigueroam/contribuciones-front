39b87babe1440548f6ffceeea6f64116ab767fa5
jmora <jmora@tgr.cl>
2019-12-17 18:03:49 +0000

    manejo moment fechas

--- src/app/domain/Cuota.ts
+++ src/app/domain/Cuota.ts
@@ -1,15 +1,11 @@
 import {CuotaDetalle} from './CuotaDetalle';
 import {Observable, Subject} from 'rxjs';
-import * as moment from 'moment';
- 
-
 
 export class Cuota {
   clasificacion: string;
   fechaVcto: Date;
   fechaVctoStr: Date;
-  fechaVctoStr1: any;
-  fechaVctoStr3: Date;
+  fechaVctoStr2: string;
   fechaVencimientoOriginal: string;
   formFolio: string;
   numeroCuota: string;
@@ -22,21 +18,18 @@ export class Cuota {
 
   changeSubject: Subject<any> = new Subject<any>();
   changeStream: Observable<any> = this.changeSubject.asObservable();
-  
-
-
 
   public constructor(init?: any) {
     this.clasificacion = "S";
     console.log("this.clasificacion", this.clasificacion);
     // this.fechaVencimiento = this.formatDate(init.fechaVencimiento);
     this.fechaVctoStr = init.fechaVcto;
-    console.log("this.fechaVctoStr", this.fechaVctoStr);
-    this.fechaVctoStr1 = moment(this.fechaVctoStr);
-    console.log("this.fechaVctoStr1", this.fechaVctoStr1);
-    
-    
-    
+    // this.fechaVctoStr2 = this.fechaVctoStr.substring(0,10);
+    this.fechaVctoStr2 = this.fechaVctoStr.toDateString();
+    console.log("this.fechaVctoStr",this.fechaVctoStr);
+    console.log("this.fechaVctoStr2",this.fechaVctoStr2);
+    // this.fechaVcto = this.formatDate(this.fechaVctoStr.substring(0,9));
+    console.log("this.fechaVcto", this.fechaVcto);
     this.fechaVencimientoOriginal = init.fechaVcto;
     // this.folio = init.folio;
     this.formFolio = init.formFolio;
