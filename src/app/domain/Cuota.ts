d6c9a3620f3c7b5937eb70cb0abbc1213198b830
jmora <jmora@tgr.cl>
2019-12-17 17:49:34 +0000

    formato de fechas

--- src/app/domain/Cuota.ts
+++ src/app/domain/Cuota.ts
@@ -1,6 +1,6 @@
 import {CuotaDetalle} from './CuotaDetalle';
 import {Observable, Subject} from 'rxjs';
-import { MomentModule } from 'ngx-moment';
+import * as moment from 'moment';
  
 
 
@@ -8,7 +8,8 @@ export class Cuota {
   clasificacion: string;
   fechaVcto: Date;
   fechaVctoStr: Date;
-  fechaVctoStr2: string;
+  fechaVctoStr1: any;
+  fechaVctoStr3: Date;
   fechaVencimientoOriginal: string;
   formFolio: string;
   numeroCuota: string;
@@ -30,12 +31,11 @@ export class Cuota {
     console.log("this.clasificacion", this.clasificacion);
     // this.fechaVencimiento = this.formatDate(init.fechaVencimiento);
     this.fechaVctoStr = init.fechaVcto;
-    // this.fechaVctoStr2 = this.fechaVctoStr.substring(0,10);
-    this.fechaVctoStr2 = this.fechaVctoStr.toDateString();
-    console.log("this.fechaVctoStr",this.fechaVctoStr);
-    console.log("this.fechaVctoStr2",this.fechaVctoStr2);
-    // this.fechaVcto = this.formatDate(this.fechaVctoStr.substring(0,9));
-    console.log("this.fechaVcto", this.fechaVcto);
+    this.fechaVctoStr1 = moment(this.fechaVctoStr);
+    console.log("this.fechaVctoStr1", this.fechaVctoStr1);
+    
+    
+    
     this.fechaVencimientoOriginal = init.fechaVcto;
     // this.folio = init.folio;
     this.formFolio = init.formFolio;
