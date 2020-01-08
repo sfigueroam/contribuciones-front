ee36a98d68545bd59f49ed4ad7570009c9f867f2
jmora <jmora@tgr.cl>
2020-01-08 18:05:37 +0000

    prueba lista codigos boton pagar

--- src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
+++ src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
@@ -31,7 +31,8 @@ export class BotonPagarComponent implements OnInit {
   ngOnInit() {
     
     this.canalRecibido = this.userdataservice.canal;
-    this.urlPagoTgr = environment.pago.url;
+    console.log(this.codigos);
+    // this.urlPagoTgr = environment.pago.url;
     
   }
 
