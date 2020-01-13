53174875808ec8bafe1a2005ea94f36e420f3c2b
jmora <jmora@tgr.cl>
2020-01-13 18:00:41 +0000

    cambio en captura de calor en boton-pagar

--- src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
+++ src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
@@ -50,7 +50,7 @@ export class BotonPagarComponent implements OnInit {
     const promesa = Promise.resolve(this.contribucionesservice.getMultiAR(this.multiAR));
     promesa.then(function(value){
       console.log(value);
-      this.cidUnico = value.codigoBarra;
+      this.cidUnico = value;
     });
     // console.log("promesa", promesa);
     
