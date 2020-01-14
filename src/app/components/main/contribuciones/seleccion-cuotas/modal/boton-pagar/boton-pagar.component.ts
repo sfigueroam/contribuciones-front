152ead95406e73c0f1e423e2e5a3a567ae42b8cf
jmora <jmora@tgr.cl>
2020-01-14 14:04:34 +0000

    pago por html

--- src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
+++ src/app/components/main/contribuciones/seleccion-cuotas/modal/boton-pagar/boton-pagar.component.ts
@@ -41,21 +41,28 @@ export class BotonPagarComponent implements OnInit {
 
     // console.log("códigos para pago", this.codigos);
     this.urlPagoTgr = environment.pago.url;
-
-  }
-  //JMS : prueba sin promesa
-  pagar(){
     this.multiAR = this.userdataservice.multiAR_Cid;
     console.log("multiAR: ", this.multiAR);
     this.contribucionesservice.postMultiaR(this.multiAR).subscribe(
       (data) => {
-        this.cidUnico = data.codigoBarra;
+        this.cidUnico = 'on, ' + data.codigoBarra + ', ';
         console.log("this.cidUnico: ", this.cidUnico);
       });
-    // Incorporación del pago
-    // this.pagoString = this.cidUnico + ',' + this.canalRecibido
-    this.contribucionesservice.postPago(this.cidUnico, this.canalRecibido).subscribe();
-  };
+
+  }
+  //JMS : prueba sin promesa
+  // pagar(){
+  //   this.multiAR = this.userdataservice.multiAR_Cid;
+  //   console.log("multiAR: ", this.multiAR);
+  //   this.contribucionesservice.postMultiaR(this.multiAR).subscribe(
+  //     (data) => {
+  //       this.cidUnico = data.codigoBarra;
+  //       console.log("this.cidUnico: ", this.cidUnico);
+  //     });
+  //   // Incorporación del pago
+  //   // this.pagoString = this.cidUnico + ',' + this.canalRecibido
+  //   this.contribucionesservice.postPago(this.cidUnico, this.canalRecibido).subscribe();
+  // };
   
   // botonPago(){
     
