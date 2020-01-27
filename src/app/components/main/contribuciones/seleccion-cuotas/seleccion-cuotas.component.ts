9ab5566dce4ecde97e8794b77801788e244903ac
jmora <jmora@tgr.cl>
2020-01-27 14:23:06 +0000

    cambio de posicion en donde se calcula el monto total

--- src/app/components/main/contribuciones/seleccion-cuotas/seleccion-cuotas.component.ts
+++ src/app/components/main/contribuciones/seleccion-cuotas/seleccion-cuotas.component.ts
@@ -252,26 +252,24 @@ export class SeleccionCuotasComponent implements OnInit, AfterViewInit {
             if (r.condonacion > 0) {
               codigos += c.liqTotal.codigoBarraTotal + ', ';
               multiARObj.listaCid.push({idMoneda:0, codigoBarra:c.liqTotal.codigoBarraTotal, montoTotal:c.liqTotal.montoTotalTotal});
-              // JMS: cambio en posicion del calculo del total
-              total += p.total;
-              console.log("total en funcion", total);
-              condonacion += p.condonacion;
             } else {
               codigos += c.liqTotal.codigoBarraParcial + ', ';
               multiARObj.listaCid.push({idMoneda:0, codigoBarra:c.liqTotal.codigoBarraParcial, montoTotal:c.liqTotal.montoTotalParcial});
-              // JMS: cambio en posicion del calculo del total
-              total += p.total;
-              console.log("total en funcion", total);
-              condonacion += p.condonacion;
             }
           }
         }
       }
     }
     // JMS: cambio en posicion del calculo del total
+    this.recalcularTipo();
+    for (const p of this.propiedades) {
+       total += p.total;
+       console.log("total en funcion", total);
+       condonacion += p.condonacion;
+    }
     this.total = total;
     this.condonacion = condonacion;
-    this.recalcularTipo();
+    
     
     this.listaContribuciones = codigos;
     multiARObj.listaCid.splice(0, 1);
